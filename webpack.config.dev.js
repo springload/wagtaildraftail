ba0137ad77f52264658d7fb49457c9492517db81
Thibaud Colas <thibaudcolas@gmail.com>
2017-02-09 12:35:20 +0000

    Move static files to static/

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -10,7 +10,7 @@ module.exports = {
         wagtaildraftail: './client/wagtaildraftail.js',
     },
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, 'static'),
         filename: '[name].bundle.js',
     },
     plugins: [
