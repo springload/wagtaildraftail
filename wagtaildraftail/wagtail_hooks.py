71a2cc9a036cee2b541b149e57d162004500bfbb
Thibaud Colas <thibaudcolas@gmail.com>
2017-02-10 11:09:59 +0000

    Add hook to load CSS

--- wagtaildraftail/wagtail_hooks.py
+++ wagtaildraftail/wagtail_hooks.py
@@ -5,5 +5,9 @@ from wagtail.wagtailcore import hooks
 
 @hooks.register('insert_editor_js')
 def draftail_editor_js():
-    return format_html('<script src="{0}"></script>',
-                       static('wagtaildraftail/wagtaildraftail.bundle.js'))
+    return format_html('<script src="{0}"></script>', static('wagtaildraftail/wagtaildraftail.js'))
+
+
+@hooks.register('insert_editor_css')
+def draftail_editor_css():
+    return format_html('<link rel="stylesheet" href="{0}">', static('wagtaildraftail/wagtaildraftail.css'))
