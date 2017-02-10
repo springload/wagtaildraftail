60dbe5c3c922c33b6f958c3c5026ae06fb5f8bd6
Thibaud Colas <thibaudcolas@gmail.com>
2017-02-10 08:34:17 +0000

    Add production webpack config

---
+++ webpack.config.prod.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const config = require('./webpack.config.dev');
+
+module.exports = Object.assign({}, config, {
+    plugins: [
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production'),
+            },
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                screw_ie8: true,
+                warnings: false
+            },
+            mangle: {
+                screw_ie8: true
+            },
+            output: {
+                comments: false,
+                screw_ie8: true
+            }
+        }),
+    ],
+});
