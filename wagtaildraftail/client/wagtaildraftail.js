628d88c0f842121cc0212d2076111e3301e535f6
Loic Teixeira <loic@springload.co.nz>
2017-04-05 15:40:52 +1200

    Move decorator and source map to separate file

--- wagtaildraftail/client/wagtaildraftail.js
+++ wagtaildraftail/client/wagtaildraftail.js
@@ -6,23 +6,8 @@ import DraftailEditor, { ENTITY_TYPE } from 'draftail';
 import 'draftail/dist/draftail.css';
 import './wagtaildraftail.css';
 
-import LinkSource from './sources/LinkSource';
-import ImageSource from './sources/ImageSource';
-import DocumentSource from './sources/DocumentSource';
-import EmbedSource from './sources/EmbedSource';
-
-import Link from './entities/Link';
-import Document from './entities/Document';
-
-const controls = {};
-controls[ENTITY_TYPE.IMAGE] = ImageSource;
-controls[ENTITY_TYPE.EMBED] = EmbedSource;
-controls[ENTITY_TYPE.LINK] = LinkSource;
-controls[ENTITY_TYPE.DOCUMENT] = DocumentSource;
-
-const decorators = {};
-decorators[ENTITY_TYPE.LINK] = Link;
-decorators[ENTITY_TYPE.DOCUMENT] = Document;
+import sources from './sources';
+import entities from './entities';
 
 // TODO: Use the one from draftail once implemented https://github.com/springload/draftail/issues/48
 const getEntityStrategy = (entityType) => {
@@ -49,9 +34,9 @@ const initDraftailEditor = (fieldName, options = {}) => {
   if (options.entityTypes) {
     // eslint-disable-next-line no-param-reassign
     options.entityTypes = options.entityTypes.map(entity => Object.assign(entity, {
-      control: controls[entity.source],
+      control: sources[entity.source],
       strategy: getEntityStrategy(entity.type),
-      component: decorators[entity.decorator],
+      component: entities[entity.decorator],
     }));
   }
 
