03d070009e58e1089a8262cf331b5a589c8d806c
Loic Teixeira <loic@springload.co.nz>
2017-04-05 16:52:18 +1200

    Pretty print raw DraftJS and HalloJS content

---
+++ tests/testapp/home/templatetags/pretty_tags.py
@@ -0,0 +1,21 @@
+import json
+
+from bs4 import BeautifulSoup
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def pretty_json(data):
+    if isinstance(data, str):
+        data = json.loads(data)
+
+    return json.dumps(data, indent=4)
+
+
+@register.filter
+def pretty_html(data):
+    soup = BeautifulSoup(data, 'html5lib')
+    body = soup.find('body')
+    return body.prettify()  # TODO: Strip the body tag.
