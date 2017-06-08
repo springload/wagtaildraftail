c11e9d7076573e7cb2dc6d90aab93151c936190d
Thibaud Colas <thibaudcolas@gmail.com>
2017-06-08 13:15:40 +0300

    Add missing source maps in dev mode

--- client/webpack/webpack.config.dev.js
+++ client/webpack/webpack.config.dev.js
@@ -3,6 +3,9 @@ const webpack = require('webpack');
 const config = require('./webpack.config.base');
 
 module.exports = Object.assign({}, config, {
+  // See http://webpack.github.io/docs/configuration.html#devtool
+  devtool: 'inline-source-map',
+
   plugins: config.plugins.concat([
     new webpack.DefinePlugin({
       'process.env': {
